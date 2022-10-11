f865f030ba21533326c44a6c6874851fd7c820d7
klukv <86426111+klukv@users.noreply.github.com>
2022-10-11 16:37:15 +0300

    customize routing(2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/footer";
 import Header from "./components/header";
 import { publicRoutes } from "./routes";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
+    <div>
+      <Header />
       <Routes>
         {publicRoutes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} />
         ))}
       </Routes>
-      <Footer/>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootElem = document.getElementById("root");
+
+if(rootElem){
+  const root = ReactDOM.createRoot(rootElem);
+root.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+);
+}
+
